ebe023b7b0c7d57a44936f1d247c60c98ccd5b3e
emerada18 <emerada18@gmail.com>
2018-11-07 10:11:14 +0100

    add here documents

--- index5.php
+++ index5.php
@@ -7,39 +7,40 @@
 </head>
 <body>   
     <?php
-    echo <<<HTML
         if(isset($_GET['name'])){
-             <div class="rc-anchor rc-anchor-normal rc-anchor-light">
-                <div class="rc-anchor-content">
-                    <div class="rc-inline-block">
-                        <div class="rc-anchor-center-container"></div>
+             echo <<<HTML1
+                 <div class="rc-anchor rc-anchor-normal rc-anchor-light">
+                    <div class="rc-anchor-content">
+                        <div class="rc-inline-block">
+                            <div class="rc-anchor-center-container"></div>
+                        </div>
+                        <div class="rc-inline-block">
+                            <div class="rc-anchor-center-container">
+                                <label class="rc-anchor-center-item rc-anchor-checkbox-label" aria-hidden="true" role="presentation" id="recaptcha-anchor-label">
+                                    <span aria-live="polite" aria-labelledby="recaptcha-accessible-status"></span>私は{$_GET['name']}ではありません
+                                </label>
+                            </div>
+                        </div>
                     </div>
-                    <div class="rc-inline-block">
-                        <div class="rc-anchor-center-container">
-                            <label class="rc-anchor-center-item rc-anchor-checkbox-label" aria-hidden="true" role="presentation" id="recaptcha-anchor-label">
-                                <span aria-live="polite" aria-labelledby="recaptcha-accessible-status"></span>
-             私は$_GET['name']ではありません
-             </label>
+                    <div class="rc-anchor-normal-footer">
+                        <div class="rc-anchor-logo-portrait" aria-hidden="true" role="presentation">
+                            <div class="rc-anchor-logo-img rc-anchor-logo-img-portrait">
+                            </div>
+                            <div class="rc-anchor-logo-text">reCAPTCHA
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="rc-anchor-normal-footer">
-                    <div class="rc-anchor-logo-portrait" aria-hidden="true" role="presentation">
-                        <div class="rc-anchor-logo-img rc-anchor-logo-img-portrait">
-                        </div>
-                        <div class="rc-anchor-logo-text">reCAPTCHA
-                        </div>
-                    </div>
-                </div>
-            </div>
+HTML1;
         }else{   
+            echo <<<HTML2
                 <h1>入力フォーム</h1>
                 <form action="index5.php" method="get">
                 <input type="text" name="name">
                 <input type="submit" value="送信">
-            </form>
+                </form>
+HTML2;
         }
-HTML;
     ?>
         
 </body>
